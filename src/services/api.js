6cada21776238d6a2e9589863b22303d222d5f27
shabelnikd <shabelnikdanil.main@gmail.com>
2025-06-15 22:27:14 +0600

    api fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Create axios instance with base URL
-const API_URL = 'http://localhost:8000/api/';
+const API_URL = 'https://rayana.diplomcomtehno.online/api/';
 
 const api = axios.create({
   baseURL: API_URL,
